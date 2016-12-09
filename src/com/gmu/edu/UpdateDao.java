57b29a9db0c64992fa65c7515024ce82565753bb
daravind54 <daravind54@gmail.com>
2016-12-09 13:22:43 -0500

    File /C:/Aravind/TempWorkSpace/BridgeCardGame/src/com/gmu/edu/UpdateDao.java changed at Fri Dec 09 13:22:43 EST 2016

--- src/com/gmu/edu/UpdateDao.java
+++ src/com/gmu/edu/UpdateDao.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 
 public class UpdateDao
 {
-	public String updateWinLoss(String email) throws ClassNotFoundException, SQLException
+	public void updateWinLoss(String email) throws ClassNotFoundException, SQLException
 	{
 		Class.forName("oracle.jdbc.driver.OracleDriver"); 
 		Connection con=DriverManager.getConnection("jdbc:oracle:thin:@apollo.vse.gmu.edu:1521:ite10g","adasari2","eecooc");
