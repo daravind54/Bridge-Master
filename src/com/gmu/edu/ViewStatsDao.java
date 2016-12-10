344bc789e85790cf1df267c700a642d2faab89a5
daravind54 <daravind54@gmail.com>
2016-12-09 21:23:16 -0500

    File /C:/Aravind/TempWorkSpace/BridgeCardGame/src/com/gmu/edu/ViewStatsDao.java changed at Fri Dec 09 21:23:16 EST 2016

--- src/com/gmu/edu/ViewStatsDao.java
+++ src/com/gmu/edu/ViewStatsDao.java
@@ -20,7 +20,8 @@ public class ViewStatsDao
 		List<StatDetails> details=new ArrayList<StatDetails>();
 		while(resultset.next())
 		{
-			
+			StatDetails s=new StatDetails();
+			s.setName(name);
 		}
 	}
 }
