3be1ec32c4e9b0b299927ddc167ca3667e6eff94
daravind54 <daravind54@gmail.com>
2016-12-09 22:02:39 -0500

    File /C:/Aravind/TempWorkSpace/BridgeCardGame/src/com/gmu/edu/ViewStatsDao.java changed at Fri Dec 09 22:02:39 EST 2016

--- src/com/gmu/edu/ViewStatsDao.java
+++ src/com/gmu/edu/ViewStatsDao.java
@@ -10,18 +10,23 @@ import java.util.List;
 
 public class ViewStatsDao 
 {
-	public void getStats(String email) throws ClassNotFoundException, SQLException
+	public List<StatDetails> getStats() throws ClassNotFoundException, SQLException
 	{
 		Class.forName("oracle.jdbc.driver.OracleDriver"); 
 		Connection con=DriverManager.getConnection("jdbc:oracle:thin:@apollo.vse.gmu.edu:1521:ite10g","adasari2","eecooc");
-		String sql="select name,email,wins,loss from userdetails";
+		String sql="select name,email,wins,loss from userdetails where email=daravind54@gmail.com";
 		Statement statement=con.createStatement();
 		ResultSet resultset=statement.executeQuery(sql);
 		List<StatDetails> details=new ArrayList<StatDetails>();
 		while(resultset.next())
 		{
 			StatDetails s=new StatDetails();
-			s.setName(name);
+			s.setName(resultset.getString("name"));
+			s.setEmail(resultset.getString("email"));
+			s.setWins(resultset.getInt("wins"));
+			s.setLoss(resultset.getInt("loss"));
+			details.add(s);
 		}
+		return details;
 	}
 }
